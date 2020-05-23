e5721b4d217e6eb0a8a8f96c0a545935c75278e6
amine-taktak <amine.taktak@hotmail.com>
2020-05-23 18:12:55 +0200

    adjusted testdata

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into user (user_id,firstname,lastname, username, password) values(1,'Jean','damme','root','1234');
-insert into user (user_id,firstname,lastname, username, password) values(2,'steven','segal','admin','1234');
-insert into user (user_id,firstname,lastname, username, password) values(3,'Armin', 'buuren','admin','1234');
-insert into user (user_id,firstname,lastname, username, password) values(4,'hannibal','barka','admin','1234');
-insert into user (user_id,firstname,lastname, username, password) values(5,'tom','cruise','admin','1234');
+insert into user (user_id,firstname,lastname, username, password) values(1,'Tom','Hanks','thanks','root');
+insert into user (user_id,firstname,lastname, username, password) values(2,'Steven','Segal','ssegal','root');
+insert into user (user_id,firstname,lastname, username, password) values(3,'Denzel', 'Washington','dWashington','root');
+insert into user (user_id,firstname,lastname, username, password) values(4,'Will','Smith','wsmith','root');
+insert into user (user_id,firstname,lastname, username, password) values(5,'tom','cruise','admin','root');
