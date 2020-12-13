2b6bb8a08d2d2c5caa0b04a30ccdb4cebc1fc23d
amine-taktak <amine.taktak@hotmail.com>
2020-12-13 22:35:00 +0100

    modified testdata

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,4 @@
-insert into user (user_id,firstname,lastname, username, password) values(1,'Tom','Hanks','thanks','root');
-insert into user (user_id,firstname,lastname, username, password) values(2,'Steven','Segal','ssegal','root');
-insert into user (user_id,firstname,lastname, username, password) values(3,'Denzel', 'Washington','dWashington','root');
-insert into user (user_id,firstname,lastname, username, password) values(4,'Will','Smith','wsmith','root');
-insert into user (user_id,firstname,lastname, username, password) values(5,'tom','cruise','admin','root');
+insert into user (user_id,firstname,lastname, username, password) values(1,'Tom','Hanks','tHanks','root');
+insert into user (user_id,firstname,lastname, username, password) values(2,'Steven','Segal','sSegal','root');
+insert into user (user_id,firstname,lastname, username, password) values(3,'Jet', 'Li','jLi','root');
+insert into user (user_id,firstname,lastname, username, password) values(4,'Will','Smith','wSmith','root');
