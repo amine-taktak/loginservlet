c0249ed6c72578aa0f9e293571357445d84f9082
amine-taktak <amine.taktak@hotmail.com>
2024-03-21 23:43:54 +0100

    adjusted testdata

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,2 @@
-insert into user (user_id,firstname,lastname, username, password) values(1,'Tom','Hanks','tHanks','root');
-insert into user (user_id,firstname,lastname, username, password) values(2,'Steven','Segal','sSegal','root');
-insert into user (user_id,firstname,lastname, username, password) values(3,'Jet', 'Li','jLi','root');
-insert into user (user_id,firstname,lastname, username, password) values(4,'Will','Smith','wSmith','root');
+insert into user (user_id,firstname,lastname, username, password) values(1,'jane','smith','jane-smith','12345');
+insert into user (user_id,firstname,lastname, username, password) values(2,'john','doe','john-doe','12345');
